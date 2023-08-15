51ea5d396cfd8dcee26b382ee9967e01d73eb49d
Vladimir Deushev <1991rand@gmail.com>
2023-08-15 10:26:32 +0300

    refactor: delete extra return

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ module.exports = function towelSort (matrix) {
   if (!matrix || matrix.length === 0) {
     return [];
   }
-  return matrix.reduce((acc, cur, i) => {
+  matrix.reduce((acc, cur, i) => {
     cur.sort((a, b) => !(i & 1) ? a - b : b - a).map(e => acc.push(e));
     return acc;
   }, []);
